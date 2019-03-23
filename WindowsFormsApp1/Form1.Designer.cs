2aa76a8fc0df7d20b36838358889aec47e2f96dd
judy-chen <vaelyra@gmail.com>
2019-03-23 04:21:54 -0700

    fixed resizing of design form, colours

--- WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -33,8 +33,8 @@ namespace WindowsFormsApp1
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
             this.numLabel = new System.Windows.Forms.Label();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.retrieve = new System.Windows.Forms.Button();
@@ -65,16 +65,6 @@ namespace WindowsFormsApp1
             this.label1.Size = new System.Drawing.Size(66, 16);
             this.label1.TabIndex = 0;
             this.label1.Text = "Input Link:";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(12, 42);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(47, 16);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "Dates:";
             // 
             // numLabel
             // 
@@ -107,6 +97,10 @@ namespace WindowsFormsApp1
             // 
             // dataGridView1
             // 
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.ControlLightLight;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.Column1,
@@ -123,8 +117,10 @@ namespace WindowsFormsApp1
             // 
             // Column1
             // 
+            this.Column1.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.DisplayedCells;
             this.Column1.HeaderText = "Name";
             this.Column1.Name = "Column1";
+            this.Column1.Width = 60;
             // 
             // Column2
             // 
@@ -174,6 +170,10 @@ namespace WindowsFormsApp1
             // 
             // dataGridView2
             // 
+            this.dataGridView2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView2.BackgroundColor = System.Drawing.SystemColors.ButtonHighlight;
             this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView2.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.Meat,
@@ -182,6 +182,7 @@ namespace WindowsFormsApp1
             this.dataGridView2.Name = "dataGridView2";
             this.dataGridView2.Size = new System.Drawing.Size(915, 212);
             this.dataGridView2.TabIndex = 8;
+            this.dataGridView2.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView2_CellContentClick);
             // 
             // Meat
             // 
@@ -209,10 +210,10 @@ namespace WindowsFormsApp1
             // DateLabel
             // 
             this.DateLabel.AutoSize = true;
-            this.DateLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
-            this.DateLabel.Location = new System.Drawing.Point(440, 41);
+            this.DateLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F);
+            this.DateLabel.Location = new System.Drawing.Point(12, 42);
             this.DateLabel.Name = "DateLabel";
-            this.DateLabel.Size = new System.Drawing.Size(39, 15);
+            this.DateLabel.Size = new System.Drawing.Size(43, 16);
             this.DateLabel.TabIndex = 11;
             this.DateLabel.Text = "Date: ";
             // 
@@ -230,8 +231,9 @@ namespace WindowsFormsApp1
             this.Controls.Add(this.retrieve);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.numLabel);
-            this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
+            this.ForeColor = System.Drawing.Color.CornflowerBlue;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -263,7 +265,6 @@ namespace WindowsFormsApp1
             {
                 if (dataGridView2.CurrentCell != null && dataGridView2.CurrentCell.Value != null)
                 {
-                    string row = (dataGridView2.CurrentCell.RowIndex + 1).ToString();
                     string path = dataGridView2.CurrentCell.Value.ToString(); 
                     Process.Start(path);
                     Console.WriteLine(path.ToString());
@@ -276,7 +277,6 @@ namespace WindowsFormsApp1
         #endregion
 
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label numLabel;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Button retrieve;
@@ -286,14 +286,14 @@ namespace WindowsFormsApp1
         private DataGridView dataGridView2;
         private DataGridViewTextBoxColumn Meat;
         private DataGridViewLinkColumn Image;
+        private Button loadsaved;
+        private Label DateLabel;
         private DataGridViewTextBoxColumn Column1;
         private DataGridViewTextBoxColumn Column2;
         private DataGridViewTextBoxColumn Column3;
         private DataGridViewTextBoxColumn Column4;
         private DataGridViewTextBoxColumn clearance;
         private DataGridViewLinkColumn Picture;
-        private Button loadsaved;
-        private Label DateLabel;
     }
 }
 
